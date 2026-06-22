22a338120a41e138b8cb3a89492af7e905284755
Serhii Rudenko <rudenko912@gmail.com>
2026-06-22 22:29:11 +0300

    alarm creates finished with event store

--- src/alarms/application/event-handlers/alarm-created.event-handler.ts
+++ src/alarms/application/event-handlers/alarm-created.event-handler.ts
@@ -2,16 +2,19 @@ import { Logger } from '@nestjs/common';
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { AlarmCreatedEvent } from '../../domain/events/alarm-created.event';
 import { UpsertMaterializedAlarmRepository } from '../ports/upsert-materialized-alarm.repository';
+import { SerializedEventPayload } from '../../../shared/domain/interfaces/serializable-event';
 
 @EventsHandler(AlarmCreatedEvent)
-export class AlarmCreatedEventHandler implements IEventHandler<AlarmCreatedEvent> {
+export class AlarmCreatedEventHandler implements IEventHandler<
+  SerializedEventPayload<AlarmCreatedEvent>
+> {
   private readonly logger = new Logger(AlarmCreatedEventHandler.name);
 
   constructor(
     private readonly upsertMaterializedAlarmRepository: UpsertMaterializedAlarmRepository,
   ) {}
 
-  async handle(event: AlarmCreatedEvent) {
+  async handle(event: SerializedEventPayload<AlarmCreatedEvent>) {
     this.logger.log(`Alarm created event: ${JSON.stringify(event)}`);
 
     // In a real-world application, we would have to ensure that this operation is atomic
@@ -22,7 +25,7 @@ export class AlarmCreatedEventHandler implements IEventHandler<AlarmCreatedEvent
       id: event.alarm.id,
       name: event.alarm.name,
       severity: event.alarm.severity.value,
-      triggeredAt: event.alarm.triggeredAt,
+      triggeredAt: new Date(event.alarm.triggeredAt),
       isAcknowledged: event.alarm.isAcknowledged,
       items: event.alarm.items,
     });
